e5f52d82fee54cbc405d278238ed7514f5fc87d0
The-Assembly <32713072+The-Assembly@users.noreply.github.com>
2018-05-30 15:46:15 +0400

    Update Infrared.py

--- Python Scripts/Infrared.py
+++ Python Scripts/Infrared.py
@@ -1,16 +1,21 @@
-import pigpio
-from time import time,sleep
+# importing the pigpio library to enable all 
+import pigpio                   
 
-irs1 = 4
-irs2 = 17
+# time library used to extract the sleep function in order to use delays
+from time import time,sleep     
 
-pi = pigpio.pi()
+irs1 = 4  #Infrared Sensor 1
+irs2 = 17 #Infrared Sensor 2
 
-pi.set_mode(irs1, pigpio.INPUT)
+pi = pigpio.pi()  # initialising the library 
+
+#Setting IR sensor as input, thereby providing sensor data through the GPIO pins
+pi.set_mode(irs1, pigpio.INPUT) 
 pi.set_mode(irs2, pigpio.INPUT)
 
 while True:
     print("IR Sensor 1: ")
-    print(pi.read(irs1))
+    print(pi.read(irs1)
     print("IR Sensor 2: ")
     print(pi.read(irs2))
+    sleep(0.5)
