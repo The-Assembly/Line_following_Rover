f3d907e0725c9bee94e9c5a7c1bc6c03cef7c053
The-Assembly <32713072+The-Assembly@users.noreply.github.com>
2018-06-01 16:53:41 +0400

    Update Line_following.py

--- Python Scripts/Line_following.py
+++ Python Scripts/Line_following.py
@@ -1,19 +1,27 @@
+# importing the pigpio  
 import pigpio
+
+# time library used to extract the sleep function in order to use delays
 from time import time,sleep
 
-ena = 11
-in1 = 9
-in2 = 10
-enb = 26
-in3 = 19
-in4 = 13
-irs1 = 4
-irs2 = 17
+#defining variables that holds a corresponding GPIO pinout 
+ena = 11 #Enable A
+in1 = 9 #Input 1
+in2 = 10 #Input 2
+enb = 26 #Enable B
+in3 = 19 #Input 3
+in4 = 13 #Input 4
+irs1 = 4  #Infrared Sensor 1
+irs2 = 17 #Infrared Sensor 2
 
+# pigpio.pi class gives access to a specified Pi's GPIO
 pi = pigpio.pi()
 
+#Setting GPIO mode to input, to access read mode 
 pi.set_mode(irs1, pigpio.INPUT)
 pi.set_mode(irs2, pigpio.INPUT)
+
+#Setting GPIO mode to output, to access write mode 
 pi.set_mode(ena,pigpio.OUTPUT)
 pi.set_mode(in1,pigpio.OUTPUT)
 pi.set_mode(in2,pigpio.OUTPUT)
@@ -21,11 +29,15 @@ pi.set_mode(enb,pigpio.OUTPUT)
 pi.set_mode(in3,pigpio.OUTPUT)
 pi.set_mode(in4,pigpio.OUTPUT)
 
-def forwards():
-    pi.set_PWM_dutycycle(ena, 50)
+def forwards(): 
+    #set_PWM_dutycycle(gpio, dutycycle)
+    #dutycycle:= 0-range (range defaults to 255)
+    #The duty cycle corresponds to speed you want to set up 
+    pi.set_PWM_dutycycle(ena, 50) 
+    pi.set_PWM_dutycycle(enb, 50)
+    #Sets the GPIO level (1-> True, 0-> False)
     pi.write(in1, 1)
     pi.write(in2, 0)
-    pi.set_PWM_dutycycle(enb, 50)
     pi.write(in3, 1)
     pi.write(in4, 0)
     print("forward")
@@ -33,29 +45,29 @@ def forwards():
 
 def backwards():
     pi.set_PWM_dutycycle(ena, 50)
-    pi.write(in1, 1)
-    pi.write(in2, 0)
     pi.set_PWM_dutycycle(enb, 50)
-    pi.write(in3, 1)
-    pi.write(in4, 0)
+    pi.write(in1, 0)
+    pi.write(in2, 1)
+    pi.write(in3, 0)
+    pi.write(in4, 1)
     print("backward")
 
 
 def right():
-    pi.set_PWM_dutycycle(ena, 55)
+    pi.write(ena, 0)
+    pi.set_PWM_dutycycle(enb, 50)
     pi.write(in1, 0)
     pi.write(in2, 0)
-    pi.set_PWM_dutycycle(enb, 55)
     pi.write(in3, 1)
     pi.write(in4, 0)
     print("right")
 
 
 def left():
-    pi.set_PWM_dutycycle(ena, 55)
+    pi.set_PWM_dutycycle(ena, 50)
+    pi.write(enb, 0)
     pi.write(in1, 1)
-    pi.write(in2, 0)
-    pi.set_PWM_dutycycle(enb, 55)
+    pi.write(in2, 0) 
     pi.write(in3, 0)
     pi.write(in4, 0)
     print("left")
@@ -63,9 +75,9 @@ def left():
 
 def stop():
     pi.write(ena, 0)
+    pi.write(enb, 0)
     pi.write(in1, 0)
     pi.write(in2, 0)
-    pi.write(enb, 0)
     pi.write(in3, 0)
     pi.write(in4, 0)
     print("nothing")
@@ -86,7 +98,6 @@ while True:
     elif(pi.read(irs1)==True and pi.read(irs2)==True): 
         stop()
 
-pigpio.cleanup()
 
 
 
